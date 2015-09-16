fa8a5cb10df9f32b856aadae6b7758d1a22c0775
Jan Debiec <jan.debiec747@gmail.com>
2015-09-16 11:08:13 +0200

    name of file fest verdrated

--- plotSpectrum.py
+++ plotSpectrum.py
@@ -3,9 +3,10 @@ import spkspectrum
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    fileName = 'spectra/0016_WA-VS_14kHz_LAI100.spk'
+    fileName = 'spectra/0016_Schichtdicke,konfokal_14kHz_LAI100.spk'
     spectrum = spkspectrum.SpkSpectrum(fileName)
-    data = spectrum.read(800)
-    plt.plot(data)
-    plt.ylabel('spectrum ' + fileName)
+    data = spectrum.read(830)
+    dataToPlot = data[16:]
+    plt.plot(dataToPlot)
+    plt.xlabel('spectrum ' + fileName)
     plt.show()
