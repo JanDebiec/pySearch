98b8a7730c394f2301e8158c373bcba00426dd90
Jan Debiec <jan.debiec747@gmail.com>
2015-09-17 07:48:30 +0200

    imlementing search level 0

--- search/datatosearch.py
+++ search/datatosearch.py
@@ -2,25 +2,64 @@ __author__ = 'jan'
 
 
 class DataToSearch:
-    def __init__(self, data):
+    def __init__(self, data, peakSeparation = 6):
         self.data = data
         self.start = 0
         self.end = len(data)
         self.actStart = 0
         self.actEnd = 0
         self.peaks = []
+        self.peakSeparation = peakSeparation
 
     def level0(self):
         self.actStart = 0
         self.actEnd = self.end
         peak = peak.peak
         self.peaks.append(peak)
-        self.searchToRight(0, -1)
+        self.firstSearchToRight(0)
 
 
 
     def searchToLeft(self, indexToFind, indexToUpdate):
         pass
+
+
+    def firstSearchToRight(self):
+        '''
+        first search, we can start from low values,
+        we don't need to check if we go down.
+        After search, peak must be validating,
+        if the peak is far away from he borders.
+        As result preak[0] will be filled .
+        :return: -1 if not found, 0 by OK
+        '''
+        i = 0
+        maxValue = 0
+        maxPosition = -1
+        for value in self.data:
+            if value > maxValue:
+                maxValue = value
+                maxPosition = i
+            i = i + 1
+        valid = self.validatePeak(maxPosition)
+        if valid:
+            self.peaks[0].maxPosition = maxPosition
+            self.peaks[0].maxValue = maxValue
+            return 0
+        else:
+            return -1
+
+
+
+    def validatePeak(selfposition):
+        '''
+
+
+        :rtype : bool
+        :param selfposition:
+        :return: bool, True if valid
+        '''
+        return True
 
 
     def searchToRight(self, indexToFind, indexToUpdate):
